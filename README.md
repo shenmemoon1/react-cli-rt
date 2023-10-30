a95e333706ece6164e6a8aa048d8492849dce9a6
billlims <davidlinxf@gmail.com>
2023-10-30 17:54:08 +1100

    update CLI App template

--- README.md
+++ README.md
@@ -12,11 +12,20 @@ To install the command globally, run the following command:
 
 ```shell
 npm install -g react-cli-rt
-
 ```
 
 To create a create react template using:
 
-> ```
-> npx react-cli-rt your-
-> ```
+```
+npx react-cli-rt your-project-name
+```
+
+To install project dependencies:
+
+`pnpm install`
+
+Run project:
+
+```
+pnpm dev
+```
--- src/features/content/Content.js
+++ src/features/content/Content.js
@@ -3,9 +3,16 @@ import dinosaur from "../../assets/dinosaur.svg";
 
 const Login = () => {
   return (
-    <div className="flex align-middle justify-center w-screen">
-      <img src={dinosaur} alt="dinosaur" />
-    </div>
+    <>
+      <div className="flex align-middle justify-center w-screen">
+        <img src={dinosaur} alt="dinosaur" />
+      </div>
+      <span className="text-center">22</span>
+      <br />
+      <button className="rounded-full w-10 h-10 mx-auto mb-4 bg-slate-500">
+        +
+      </button>
+    </>
   );
 };
 
